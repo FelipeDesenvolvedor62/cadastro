2be83cdefc8804d4c8559f74679283f490aa35cb
FelipeDesenvolvedor62 <felipe.a.desenvolvimento62@gmail.com>
2022-01-23 17:25:12 -0300

    Herança JPA

--- src/main/java/com/br/felipedev/cadastro/controller/ClienteController.java
+++ src/main/java/com/br/felipedev/cadastro/controller/ClienteController.java
@@ -1,6 +1,7 @@
 package com.br.felipedev.cadastro.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.felipedev.cadastro.Service.ClienteService;
@@ -22,8 +22,8 @@ public class ClienteController {
 	private ClienteService service;
 
 	@GetMapping("/{id}")
-	public Cliente findById(@PathVariable Long Id) {
-		return null;
+	public Optional<Cliente> findById(@PathVariable Long id) {
+		return service.findById(id);
 	}
 
 	@GetMapping
@@ -33,7 +33,6 @@ public class ClienteController {
 
 	@PostMapping
 	public void add(@RequestBody Cliente cliente) {
-		System.out.println(cliente.getNome());
 		service.add(cliente);
 	}
 }
--- src/main/java/com/br/felipedev/cadastro/model/Cliente.java
+++ src/main/java/com/br/felipedev/cadastro/model/Cliente.java
@@ -4,24 +4,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
 
-import org.hibernate.validator.constraints.Length;
-
-import com.br.felipedev.cadastro.model.ENUM.TipoTelefone;
+import com.br.felipedev.cadastro.model.abstracts.Pessoa;
 
 @Entity
-public class Cliente {
+public class Cliente extends Pessoa {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-
-	
-	@Length(min=2, max=100, message="O campo deve conter entre 5 a 80 caracteres")
-	@NotBlank(message = "Não é permidito cadastrar somente espaços em branco")
-	private String nome;
-	
+	private Long id;	
+	private Long matricula;
 	public Long getId() {
 		return id;
 	}
@@ -29,21 +21,21 @@ public class Cliente {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-	public String getNome() {
-		return nome;
+	
+	public Long getMatricula() {
+		return matricula;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setMatricula(Long matricula) {
+		this.matricula = matricula;
 	}
 
 	public Cliente() {
 
 	}
 
-	public Cliente(String nome) {
-		this.nome = nome;
+	public Cliente(Long matricula) {
+		this.matricula = matricula;
 	}
 
 }
